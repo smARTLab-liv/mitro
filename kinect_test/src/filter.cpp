310a36b47128770bf15e3f7814e0953d609e4a98
Daniel Hennes <daniel.hennes@gmail.com>
2011-10-21 17:36:26 +0000

    kinect water-level

--- kinect_test/src/filter.cpp
+++ kinect_test/src/filter.cpp
@@ -8,14 +8,16 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/project_inliers.h>
+#include <pcl/filters/radius_outlier_removal.h>
 
 int main (int argc, char** argv) 
 {
 
   pcl::PointCloud<pcl::PointXYZRGB> rgb_cloud;
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>),
-    cloud_floor (new pcl::PointCloud<pcl::PointXYZ>);
-
+    cloud_floor (new pcl::PointCloud<pcl::PointXYZ>),
+    cloud_without_floor (new pcl::PointCloud<pcl::PointXYZ>),
+    cloud_without_floor_filtered(new pcl::PointCloud<pcl::PointXYZ>);
 
   std::string fn = "cloud_84.pcd";
   if (argc > 1)
@@ -45,7 +47,7 @@ int main (int argc, char** argv)
   // Mandatory
   seg.setModelType (pcl::SACMODEL_PLANE);
   seg.setMethodType (pcl::SAC_RANSAC);
-  seg.setDistanceThreshold (0.05);
+  seg.setDistanceThreshold (0.02);
 
   // seg.setInputCloud (cloud.makeShared ());
   // seg.segment (*inliers, *coefficients);
@@ -71,28 +73,56 @@ int main (int argc, char** argv)
   // Extract the inliers
   extract.setInputCloud (cloud);
   extract.setIndices (inliers);
+  extract.setNegative (false);
+  extract.filter (*cloud_floor);
+
+
+  extract.setInputCloud (cloud);
+  extract.setIndices (inliers);
   extract.setNegative (true);
   extract.filter (*cloud_without_floor);
+
+
+  pcl::RadiusOutlierRemoval<pcl::PointXYZ> outlier_filter;
+  outlier_filter.setInputCloud(cloud_without_floor);
+  outlier_filter.setRadiusSearch(0.1f);
+  outlier_filter.setMinNeighborsInRadius(25);
+  outlier_filter.filter(*cloud_without_floor_filtered);
+
+
+  int num_p_wo_floor = cloud_without_floor->width *
+    cloud_without_floor->height;
+
+  int num_p_filtered = cloud_without_floor_filtered->width *
+    cloud_without_floor_filtered->height;
+
+  ROS_INFO("outlier filtering removed %d points. final cloud has %d points.", (num_p_wo_floor - num_p_filtered), num_p_filtered);
+
+
 
   ROS_INFO("done!");
 
 
   coefficients->values[3] += 0.1;
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::ProjectInliers<pcl::PointXYZ> proj;
-  proj.setModelType (pcl::SACMODEL_PLANE);
-  proj.setInputCloud (cloud);
-  proj.setModelCoefficients (coefficients);
-  proj.filter (*cloud_projected);
+  // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
+  // pcl::ProjectInliers<pcl::PointXYZ> proj;
+  // proj.setModelType (pcl::SACMODEL_PLANE);
+  // proj.setInputCloud (cloud);
+  // proj.setModelCoefficients (coefficients);
+  // proj.filter (*cloud_projected);
+
+
 
 
   std::cerr << "PointCloud representing the planar component: " << cloud_floor->width * cloud_floor->height << " data points." << std::endl;
 
+  pcl::io::savePCDFileASCII("cloud_floor.pcd", *cloud_floor);
+  pcl::io::savePCDFileASCII("cloud_without_floor.pcd", *cloud_without_floor);
 
-  std::string out_fn = "output.pcd";
-  pcl::io::savePCDFileASCII(out_fn.c_str(), *cloud_floor);
-
+  pcl::io::savePCDFileASCII("cloud_without_floor_filtered.pcd", *cloud_without_floor_filtered);
+  
+  pcl::io::savePCDFileASCII("cloud.pcd", *cloud);
 
 
       // // Create the filtering object
--- kinect_test/src/voxel_grid.cpp
+++ kinect_test/src/voxel_grid.cpp
@@ -5,14 +5,34 @@
 #include "sensor_msgs/LaserScan.h"
 
 #include <pcl/point_types.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/passthrough.h>
+
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/ModelCoefficients.h>
+
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/project_inliers.h>
 #include <pcl/filters/radius_outlier_removal.h>
-#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
 
-float static VOXEL_SCALE = 0.025f;
+#include <dynamic_reconfigure/server.h>
+#include <kinect_test/KinectTestConfig.h>
+
+#include <tf/transform_broadcaster.h>
+
+#define PI 3.1415f
+
 double static MAX_UPDATE_RATE = 10.0;
+float static KINECT_PITCH_ANGLE = 0.41;
 
+float voxel_size = 0.025f;
+float plane_tresh = 0.02f;
+
+
+int outlier_neighbors = 10;
+float outlier_radius = 0.1f;
 
 ros::Subscriber sub;
 ros::Publisher pub, pub_voxel, pub_scan;
@@ -21,6 +41,27 @@ ros::Time last_update;
 sensor_msgs::PointCloud2::Ptr last_cloud;
 
 int count = 0;
+
+
+
+// dynamic reconfigure shit goes here
+void callback(kinect_test::KinectTestConfig &config, uint32_t level) {
+  ROS_DEBUG("dyn reconf call (voxel_size, plane_tresh, outlier_radius, outlier_neighbors): %f %f %f %d", 
+	    config.voxel_size,
+	    config.plane_tresh,
+	    config.outlier_radius,
+	    config.outlier_neighbors);
+  voxel_size = (float) config.voxel_size;
+  plane_tresh = (float) config.plane_tresh;
+  outlier_radius = (float) config.outlier_radius;
+  outlier_neighbors = config.outlier_neighbors;
+}
+
+
+float radToDeg(float a)
+{
+  return a / PI * 180.0f;
+}
 
 void cb_pointcloud2(const sensor_msgs::PointCloud2::Ptr cloud_in)
 {
@@ -52,7 +93,6 @@ sensor_msgs::LaserScan::Ptr pointcloud_to_laser(sensor_msgs::PointCloud2::Ptr cl
   pcl::PointCloud<pcl::PointXYZRGB> pcl_cloud;
   pcl::fromROSMsg(*cloud, pcl_cloud);
 
-  ROS_INFO("pcl points %ld", pcl_cloud.points.size());
   if ( 0 == pcl_cloud.points.size() )
     return scan;
 
@@ -83,32 +123,87 @@ sensor_msgs::LaserScan::Ptr pointcloud_to_laser(sensor_msgs::PointCloud2::Ptr cl
 
 void filter(sensor_msgs::PointCloud2::Ptr cloud_in)
 {
-  sensor_msgs::PointCloud2::Ptr cloud_voxel (new sensor_msgs::PointCloud2());
-  sensor_msgs::PointCloud2::Ptr cloud_pass_z (new sensor_msgs::PointCloud2());
-  sensor_msgs::PointCloud2::Ptr cloud_pass_x (new sensor_msgs::PointCloud2());
-  sensor_msgs::PointCloud2::Ptr cloud_outlier (new sensor_msgs::PointCloud2());
+  sensor_msgs::PointCloud2::Ptr cloud_voxel (new sensor_msgs::PointCloud2);
+  // sensor_msgs::PointCloud2::Ptr cloud_pass_z (new sensor_msgs::PointCloud2());
+  // sensor_msgs::PointCloud2::Ptr cloud_pass_x (new sensor_msgs::PointCloud2());
+  //sensor_msgs::PointCloud2::Ptr cloud_outlier (new sensor_msgs::PointCloud2());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_without_floor (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_without_floor_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  
+  // pcl::PassThrough<sensor_msgs::PointCloud2> pass_through_x;
+  // pass_through_x.setInputCloud(cloud_voxel);
+  // pass_through_x.setFilterFieldName("x"); // kinext is rotated 90 deg
+  // pass_through_x.setFilterLimits(-1.1, 0.5);
+  // pass_through_x.filter(*cloud_pass_x);
+
+  // pub_voxel.publish(cloud_voxel);
+
 
   pcl::VoxelGrid<sensor_msgs::PointCloud2> voxel_filter;
   voxel_filter.setInputCloud (cloud_in);
-  voxel_filter.setLeafSize (VOXEL_SCALE, VOXEL_SCALE, VOXEL_SCALE);
+  voxel_filter.setLeafSize (voxel_size, voxel_size, voxel_size);
   voxel_filter.filter (*cloud_voxel);
-  
-  pcl::PassThrough<sensor_msgs::PointCloud2> pass_through_x;
-  pass_through_x.setInputCloud(cloud_voxel);
-  pass_through_x.setFilterFieldName("x"); // kinext is rotated 90 deg
-  pass_through_x.setFilterLimits(-1.1, 0.5);
-  pass_through_x.filter(*cloud_pass_x);
 
   pub_voxel.publish(cloud_voxel);
 
+  pcl::fromROSMsg(*cloud_voxel, *pcl_cloud);
 
-  pcl::PointCloud<pcl::PointXYZ> pcl_cloud;
-  pcl::fromROSMsg(*cloud_voxel, pcl_cloud);
-
-  if ( 0 == pcl_cloud.points.size() )
+  if ( 0 == pcl_cloud->points.size() )
+  {
+    ROS_ERROR("Cloud is empty!");
     return;
+  }
 
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+
+  // Create the segmentation object
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  // Optional
+  seg.setOptimizeCoefficients (true);
+  // Mandatory
+  seg.setModelType (pcl::SACMODEL_PLANE);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setDistanceThreshold (plane_tresh);
+  seg.setInputCloud (pcl_cloud);
+  seg.segment (*inliers, *coefficients);
+
+  float x = -coefficients->values[0];
+  float y = -coefficients->values[1];
+  float z = -coefficients->values[2];
+
+
+
+  //  ROS_INFO("coefficients: %f %f %f %f", coefficients->values[0], coefficients->values[1], coefficients->values[2], coefficients->values[3]);
+
+
+  float roll = atan2(z,-x) - PI/2.0f;
+  float pitch = atan2(z,y) - PI/2.0f + KINECT_PITCH_ANGLE;
+
+  ROS_INFO("roll: %f", radToDeg(roll));
+  ROS_INFO("pitch: %f", radToDeg(pitch));
    
+
+  if (inliers->indices.size () == 0)
+  {
+    ROS_ERROR("Could not estimate a planar model for the given dataset!");
+    return;
+  }
+
+  pcl::ExtractIndices<pcl::PointXYZ> extract;
+  extract.setInputCloud (pcl_cloud);
+  extract.setIndices (inliers);
+  extract.setNegative (false);
+  extract.filter (*cloud_without_floor);
+
+  pcl::RadiusOutlierRemoval<pcl::PointXYZ> outlier_filter;
+  outlier_filter.setInputCloud(cloud_without_floor);
+  outlier_filter.setRadiusSearch(outlier_radius);
+  outlier_filter.setMinNeighborsInRadius(outlier_neighbors);
+  outlier_filter.filter(*cloud_without_floor_filtered);
+
+  //  pub_voxel.publish(cloud_without_floor_filtered);
 
   // char str [200];
   // sprintf(str, "cloud_%d.pcd", ++count);
@@ -138,14 +233,30 @@ void filter(sensor_msgs::PointCloud2::Ptr cloud_in)
 
   // pub.publish(cloud_outlier);
 
+  sensor_msgs::PointCloud2::Ptr cloud_out (new sensor_msgs::PointCloud2);
+  pcl::toROSMsg(*cloud_without_floor_filtered, *cloud_out);
+  pub.publish(cloud_out);
+
   // pub_scan.publish(pointcloud_to_laser(cloud_outlier));
 
+
+  static tf::TransformBroadcaster br;
+  tf::Transform transform;
+  transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+  transform.setRotation( tf::Quaternion(0, 0, 0) );
+  br.sendTransform(tf::StampedTransform(transform, cloud_in->header.stamp, "/bob/odom", "kinect_frame"));
 }
 
 int main (int argc, char** argv)
 {
   ros::init(argc, argv, "kinect_test");
   ros::NodeHandle n;
+
+  dynamic_reconfigure::Server<kinect_test::KinectTestConfig> server;
+  dynamic_reconfigure::Server<kinect_test::KinectTestConfig>::CallbackType f;
+
+  f = boost::bind(&callback, _1, _2);
+  server.setCallback(f);
 
   sub = n.subscribe("/cloud_in", 1, cb_pointcloud2);
   pub = n.advertise<sensor_msgs::PointCloud2>("/cloud_out", 1);
