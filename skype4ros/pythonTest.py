869d23c35b6d9609bbef1b1bf41b284a149ee3ab
Sjriek Alers <sjriek.alers@gmail.com>
2011-10-31 09:09:37 +0000

    delete

--- skype4ros/pythonTest.py
+++
@@ -1,8 +0,0 @@
-#Program received signal SIGSEGV, Segmentation fault.
-#[Switching to Thread 0x7ffff4293700 (LWP 3079)]
-#0x00007ffff5feebfc in ?? () from /lib/x86_64-linux-gnu/libdbus-1.so.3
-import Skype4Py
-s=Skype4Py.Skype()
-s.Attach()
-s.PlaceCall("kroumans")
-
