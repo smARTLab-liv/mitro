77b120a40be28c3efb5ac08696dabceaabd43f5b
Daan Bloembergen <daan.bloembergen@gmail.com>
2011-10-31 08:48:24 +0000

    fixed assisted_drive bug

--- mitro_apps/assisted_drive/src/assisted_drive.cpp
+++ mitro_apps/assisted_drive/src/assisted_drive.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Point.h>
+#include <std_msgs/Bool.h>
 #include <nav_msgs/GridCells.h>
 #include <tf/transform_listener.h>
 
@@ -15,62 +16,78 @@ tf::TransformListener *tf_listener;
 bool obst_detected = false;
 ros::Time last_costmap;
 
+bool use_assisted_drive = true;
+
 double modulus(double a, double b);
 
 
+void cmd_ad_cb(const std_msgs::Bool::ConstPtr& msg) {
+    use_assisted_drive = msg->data;
+    if (use_assisted_drive) ROS_DEBUG("Assisted drive mode enabled");
+    else ROS_DEBUG("Assisted drive mode disabled");
+}
+
 void twist_cb(const geometry_msgs::Twist::ConstPtr& msg) {
-    geometry_msgs::Twist twist_msg;
-    if ( (ros::Time::now() - last_costmap).toSec() > 1.0) {
-        ROS_ERROR("Haven't seen any costmap in the last second!");
-        twist_msg.linear.x = 0;
-        twist_msg.angular.z = msg->angular.z;
-    }
-    else if (obst_detected and msg->linear.x > 0.0) {
-        ROS_ERROR("Obstacle detected!");
-        twist_msg.linear.x = 0;
-        twist_msg.angular.z = msg->angular.z;
+    if (use_assisted_drive) {
+        geometry_msgs::Twist twist_msg;
+        if ( (ros::Time::now() - last_costmap).toSec() > 1.0) {
+            ROS_ERROR("Haven't seen any costmap in the last second!");
+            twist_msg.linear.x = 0;
+            twist_msg.angular.z = msg->angular.z;
+        }
+        else if (obst_detected and msg->linear.x > 0.0) {
+            ROS_ERROR("Obstacle detected!");
+            twist_msg.linear.x = 0;
+            twist_msg.angular.z = msg->angular.z;
+        }
+        else {
+            twist_msg.linear.x = msg->linear.x;
+            twist_msg.angular.z = msg->angular.z;
+        }
+        twist_pub.publish(twist_msg);
     }
     else {
-        twist_msg.linear.x = msg->linear.x;
-        twist_msg.angular.z = msg->angular.z;
+        twist_pub.publish(*msg);
     }
-    twist_pub.publish(twist_msg);
 }
 
 void costmap_cb(const nav_msgs::GridCells::ConstPtr& msg) {
-    ODOM_FRAME = msg->header.frame_id;
-    
-    last_costmap = msg->header.stamp;
-    std::vector<geometry_msgs::Point> points = msg->cells;
-    
-    for (std::vector<std::string>::size_type i = 0; i < points.size(); i++) {
-        geometry_msgs::PointStamped point, point_trans;
-        point.header.stamp = msg->header.stamp;
-        point.header.frame_id = ODOM_FRAME;
-        point.point.x = points[i].x;
-        point.point.y = points[i].y;
-        tf_listener->waitForTransform(BASE_FRAME, ODOM_FRAME, point.header.stamp, ros::Duration(0.1));
-        try {
-            tf_listener->transformPoint(BASE_FRAME, point, point_trans);
-        }
-        catch (tf::ExtrapolationException e) {
-            ROS_ERROR("Unable to get tf transform: %s", e.what());
-            return;
-        }
+    if (use_assisted_drive) {
+        ODOM_FRAME = msg->header.frame_id;
         
-        float x = point_trans.point.x;
-        float y = point_trans.point.y;
-        float dist = sqrt(pow(x, 2) + pow(y, 2));
-        //float yaw = atan(y / x);
-        //if (x > 0) yaw = modulus(yaw + PI, 2*PI);
+        last_costmap = msg->header.stamp;
+        std::vector<geometry_msgs::Point> points = msg->cells;
         
-        if (dist < BASE_RADIUS + CLEARING_DIST and x > 0) {
-            obst_detected = true;
-        }
-        else {
-            obst_detected = false;
+        for (std::vector<std::string>::size_type i = 0; i < points.size(); i++) {
+            geometry_msgs::PointStamped point, point_trans;
+            point.header.stamp = msg->header.stamp;
+            point.header.frame_id = ODOM_FRAME;
+            point.point.x = points[i].x;
+            point.point.y = points[i].y;
+            tf_listener->waitForTransform(BASE_FRAME, ODOM_FRAME, point.header.stamp, ros::Duration(0.1));
+            try {
+                tf_listener->transformPoint(BASE_FRAME, point, point_trans);
+            }
+            catch (tf::ExtrapolationException e) {
+                ROS_ERROR("Unable to get tf transform: %s", e.what());
+                return;
+            }
+            
+            float x = point_trans.point.x;
+            float y = point_trans.point.y;
+            float dist = sqrt(pow(x, 2) + pow(y, 2));
+            //float yaw = atan(y / x);
+            //if (x > 0) yaw = modulus(yaw + PI, 2*PI);
+            
+            if (dist < BASE_RADIUS + CLEARING_DIST and x > 0) {
+                obst_detected = true;
+            }
+            else {
+                obst_detected = false;
+            }
         }
     }
+    else obst_detected = false;
 }
 
 double modulus(double a, double b) {
@@ -104,6 +121,7 @@ int main(int argc, char** argv){
     
     ros::Subscriber twist_sub = nh.subscribe<geometry_msgs::Twist>(source_topic, 10, twist_cb);
     ros::Subscriber costmap_sub = nh.subscribe<nav_msgs::GridCells>(costmap_topic, 10, costmap_cb);
+    ros::Subscriber cmd_ad_sub = nh.subscribe<std_msgs::Bool>("cmd_assisted_drive", 10, cmd_ad_cb);
     twist_pub = nh.advertise<geometry_msgs::Twist>(target_topic, 10);
 
     ros::spin();
