387b3f2181e6b11a0afb7e25829b118ae8b0b6af
Daan Bloembergen <daan.bloembergen@gmail.com>
2012-02-29 12:36:06 +0000

    ??

--- mitro/mitro_bringup/mitro_bringup.sh
+++ mitro/mitro_bringup/mitro_bringup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 source /opt/ros/electric/setup.bash
 export ROS_PACKAGE_PATH=/home/swarmlab/ros:$ROS_PACKAGE_PATH
-roslaunch --pid=/home/swarmlab/mitro_bringup.pid mitro_bringup minimal.launch
+roslaunch --pid=/home/swarmlab/mitro_bringup.pid mitro_bringup minimal.launch &
+sleep 8 && roslaunch --pid=/home/swarmlab/mitro_bob.pid mitro_bringup bnaic_bob.launch
+
+
+
