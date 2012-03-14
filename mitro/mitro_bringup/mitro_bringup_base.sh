dbeb49c638f2382e96bf5db751296944eb5663aa
Daan Bloembergen <daan.bloembergen@gmail.com>
2012-03-14 15:31:29 +0000

    roscore starts independent of other nodes

--- mitro/mitro_bringup/mitro_bringup_base.sh
+++ mitro/mitro_bringup/mitro_bringup_base.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 source /opt/ros/electric/setup.bash
 export ROS_PACKAGE_PATH=/home/swarmlab/ros:$ROS_PACKAGE_PATH
-roslaunch --pid=/home/swarmlab/mitro_bringup_base.pid mitro_bringup base.launch
+
+DONE=0
+while [ $DONE -lt 1 ] ; do
+	if ps ax | grep -v grep | grep roscore > /dev/null
+	then
+		roslaunch --pid=/home/swarmlab/mitro_bringup_base.pid mitro_bringup base.launch
+    		DONE=1
+	else
+		echo "no roscore started... waiting"
+	fi
+done
---
+++ mitro/mitro_bringup/mitro_bringup_core.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /opt/ros/electric/setup.bash
+export ROS_PACKAGE_PATH=/home/swarmlab/ros:$ROS_PACKAGE_PATH
+roscore
