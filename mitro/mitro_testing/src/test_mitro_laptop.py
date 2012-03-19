4003733ace0869d5d5cfbc7e22a8c7fea0793430
Daniel Hennes <daniel.hennes@gmail.com>
2012-03-19 14:56:52 +0000

    mitro_laptop_test

--- mitro/mitro_testing/src/test_mitro_laptop.py
+++ mitro/mitro_testing/src/test_mitro_laptop.py
@@ -6,18 +6,21 @@ def test_network():
     network_tests = []
     add_test( network_tests , network.check_ip, 'eth0', '10.10.14.2' )
     add_test( network_tests , network.check_unconnected, 'wlan0' )
-    add_test( network_tests , network.check_ip, 'wlan1', '10.10.10.11' )
+    add_test( network_tests , network.ping, 'localhost' )
     add_test( network_tests , network.ping, 'mitro-laptop' )
     add_test( network_tests , network.ping, 'bob' )
     add_test( network_tests , network.ping, 'www.google.com' )
-    add_test( network_tests , network.ping, 'swarmlab.unimaas.nl' )
+    add_test( network_tests , network.ping, 'root' )
     group_test(network_tests)
 
 def test_video():
     video_tests = []
-    add_test( video_tests , video.check_device, '/dev/video0' )
     add_test( video_tests , video.check_device, '/dev/video1' )
-    add_test( video_tests , video.check_v4l_info, '/dev/video0', 'uvcvideo' )
+    add_test( video_tests , video.check_device, '/dev/video2' )
+    add_test( video_tests , video.check_device, '/dev/video3' )
+    add_test( video_tests , video.check_v4l_info, '/dev/video1', 'uvcvideo' )
+    add_test( video_tests , video.check_v4l_info, '/dev/video2', 'uvcvideo' )
+    add_test( video_tests , video.check_v4l_info, '/dev/video3', 'Dummy' )
     group_test(video_tests)
 
 if __name__ == '__main__':
