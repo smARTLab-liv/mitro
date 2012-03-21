71e05704a321cbaa5bb8877d4ecfea608e5861e3
Daniel Hennes <daniel.hennes@gmail.com>
2012-03-21 14:02:49 +0000

    base test

--- mitro/mitro_testing/src/test_mitro_base.py
+++ mitro/mitro_testing/src/test_mitro_base.py
@@ -6,6 +6,7 @@ def test_network():
     print '-'*80
     network_tests = []
     add_test(network_tests, network.ip, 'eth0', '10.10.14.1')
+    add_test(network_tests, network.ip, 'wlan0', '10.10.10.10')
     add_test(network_tests, network.ping, 'localhost')
     add_test(network_tests, network.ping, 'bob')
     add_test(network_tests, network.ping, 'mitro-laptop')
@@ -18,7 +19,8 @@ def test_ros():
     ros_tests = []
     add_test(ros_tests, ros.roscore, )
     add_test(ros_tests, ros.rostopichz, '/base_scan', 10)
-#    add_test(ros_tests, ros.rostopichz, '/cloud_obstacles', 10)
+    add_test(ros_tests, ros.rostopichz, '/cloud_obstacles', 10)
+    add_test(ros_tests, ros.rostopichz, '/joint_states', 15)
     group_test(ros_tests)
 
 if __name__ == '__main__':
