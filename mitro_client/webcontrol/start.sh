87ab202114e9206a9fff33d785d3e7e22a17f24f
Daniel Hennes <daniel.hennes@gmail.com>
2012-03-21 15:39:17 +0000

    fucking daemon script works now finallysvn statussvn status

--- mitro_client/webcontrol/start.sh
+++ mitro_client/webcontrol/start.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-export DISPLAY=:0.0 && src/server.py --host 10.10.14.2 --port 8080
+PID_FILE="/home/swarmlab/webcontrol.pid"
+LOG_FILE="/home/swarmlab/webcontrol.log"
+rm -f $PID_FILE
+rm -f $LOG_FILE
+
+source /opt/ros/electric/setup.bash
+export ROS_MASTER_URI=http://bob:11311
+export DISPLAY=:0.0
+
+/home/swarmlab/ros/mitro_client/webcontrol/src/webcontrol.py --host 10.10.14.2 --port 8080 &> $LOG_FILE &
+
+ps aux | grep webcontrol.py | grep -v grep | awk '{print $2}' >> $PID_FILE
+
+
