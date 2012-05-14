ddc06a2c0cfa752a132c0c6c9858ab1e8b1df346
Daan Bloembergen <daan.bloembergen@gmail.com>
2012-05-14 15:14:29 +0000

    sonar laser

--- mitro/mitro_sonar/src/mitro_sonar.cpp
+++ mitro/mitro_sonar/src/mitro_sonar.cpp
@@ -1,27 +1,34 @@
 #include <ros/ros.h>
 #include <mitro_sonar/SonarScan.h>
 #include <mitro_sonar/SonarRaw.h>
+#include <sensor_msgs/LaserScan.h>
 
 #include <math.h>
 
 #define PI 3.1415926536
 
 ros::Publisher sonar_pub;
+ros::Publisher laser_pub;
 
 std::vector<float> ranges, angles;
-double min_range, max_range, arc_range;
+double min_range, max_range, arc_range, base_radius;
 
 mitro_sonar::SonarScan sonar_msg;
+sensor_msgs::LaserScan laser_msg;
 
 void sonar_raw_cb(const mitro_sonar::SonarRaw::ConstPtr& msg) {
     sonar_msg.header.stamp = ros::Time::now();
-    ranges[0] = msg->s1_dist;
-    ranges[1] = msg->s2_dist;
-    ranges[2] = msg->s3_dist;
-    ranges[3] = msg->s4_dist;
-    ranges[4] = msg->s5_dist;
+    ranges[0] = msg->s1_dist + base_radius;
+    ranges[1] = msg->s2_dist + base_radius;
+    ranges[2] = msg->s3_dist + base_radius;
+    ranges[3] = msg->s4_dist + base_radius;
+    ranges[4] = msg->s5_dist + base_radius;
     sonar_msg.ranges = ranges;
     sonar_pub.publish(sonar_msg);
+    
+    laser_msg.header.stamp = ros::Time::now();
+    laser_msg.ranges = ranges;
+    laser_pub.publish(laser_msg);
 }
 
 int main(int argc, char** argv) {
@@ -30,6 +37,7 @@ int main(int argc, char** argv) {
 
   ros::Subscriber sonar_sub = n.subscribe("sonar_raw", 10, sonar_raw_cb);
   sonar_pub = n.advertise<mitro_sonar::SonarScan>("sonar_scan", 10);
+  laser_pub = n.advertise<sensor_msgs::LaserScan>("sonar_laser", 10);
   
   ranges = std::vector<float>(5);
     
@@ -43,6 +51,8 @@ int main(int argc, char** argv) {
     n.getParam("mitro_sonar/sonar_min", min_range);
     n.getParam("mitro_sonar/sonar_max", max_range);
     n.getParam("mitro_sonar/sonar_arc", arc_range);
+    n.getParam("base_diameter", base_radius);
+    base_radius /= 2.0;
     
     angles = std::vector<float>(5);
     angles[0] = s1_angle;
@@ -56,6 +66,12 @@ int main(int argc, char** argv) {
     sonar_msg.min_range = min_range;
     sonar_msg.max_range = max_range;
     sonar_msg.angles = angles;
+    
+    laser_msg.header.frame_id = "base_link";
+    laser_msg.angle_min = s1_angle;
+    laser_msg.angle_max = s5_angle;
+    laser_msg.angle_increment = 0.5;   
+    
   }
   catch (ros::InvalidNameException e) {
     ROS_ERROR("Parameter not set: %s", e.what());
