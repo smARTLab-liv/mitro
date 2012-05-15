721ee4cdff4feaa7541f21ef3bafcb361dac8457
Daan Bloembergen <daan.bloembergen@gmail.com>
2012-05-15 09:50:16 +0000

    assisted drive uses sonar data

--- mitro_apps/assisted_drive/src/assisted_drive.cpp
+++ mitro_apps/assisted_drive/src/assisted_drive.cpp
@@ -4,18 +4,21 @@
 #include <std_msgs/Bool.h>
 #include <nav_msgs/GridCells.h>
 #include <tf/transform_listener.h>
+#include <mitro_sonar/SonarScan.h>
 
 #define PI 3.141592653589793
 
 double BASE_RADIUS;
 double CLEARING_DIST;
+double SONAR_CLEARING_DIST;
 std::string BASE_FRAME, ODOM_FRAME;
 
 ros::Publisher twist_pub, status_pub;
 tf::TransformListener *tf_listener;
 bool obst_detected = false;
 bool obst_stop = false;
-ros::Time last_costmap, last_update;
+bool sonar_stop = false;
+ros::Time last_costmap, last_update, last_sonar;
 
 bool use_assisted_drive = false;
 
@@ -31,26 +34,39 @@ void cmd_ad_cb(const std_msgs::Bool::ConstPtr& msg) {
 void twist_cb(const geometry_msgs::Twist::ConstPtr& msg) {
     if (use_assisted_drive) {
         geometry_msgs::Twist twist_msg;
-        if ( (ros::Time::now() - last_costmap).toSec() > 1.0) {
-            ROS_ERROR("Haven't seen any costmap in the last second!");
-            twist_msg.linear.x = 0;
-            twist_msg.angular.z = msg->angular.z;
+        twist_msg.linear.x = msg->linear.x;
+        twist_msg.angular.z = msg->angular.z;
+                    
+        if (msg->linear.x > 0.0) {
+            if ( (ros::Time::now() - last_costmap).toSec() > 1.0) {
+                ROS_ERROR("Haven't seen any costmap in the last second!");
+                twist_msg.linear.x = 0;
+                twist_msg.angular.z = msg->angular.z;
+            }
+            else if (obst_detected) {
+                ROS_DEBUG("Obstacle detected!");
+                if (obst_stop) {
+                    ROS_DEBUG("STOP!");
+                    twist_msg.linear.x = 0;
+                    twist_msg.angular.z = msg->angular.z;
+                }
+                else {
+                    twist_msg.linear.x = 0.2;
+                    twist_msg.angular.z = msg->angular.z;
+                }
+            }
         }
-        else if (obst_detected && msg->linear.x > 0.0) {
-            ROS_DEBUG("Obstacle detected!");
-            if (obst_stop) {
+        else if (msg->linear.x < 0.0) {
+            if ( (ros::Time::now() - last_sonar).toSec() > 1.0) {
+                ROS_ERROR("Haven't seen any sonar data in the last second!");
+                twist_msg.linear.x = 0;
+                twist_msg.angular.z = msg->angular.z;
+            }
+            else if (sonar_stop) {
                 ROS_DEBUG("STOP!");
                 twist_msg.linear.x = 0;
                 twist_msg.angular.z = msg->angular.z;
             }
-            else {
-                twist_msg.linear.x = 0.2;
-                twist_msg.angular.z = msg->angular.z;
-            }
-        }
-        else {
-            twist_msg.linear.x = msg->linear.x;
-            twist_msg.angular.z = msg->angular.z;
         }
         twist_pub.publish(twist_msg);
     }
@@ -97,6 +113,20 @@ void costmap_cb(const nav_msgs::GridCells::ConstPtr& msg) {
     }
 }
 
+void sonar_cb(const mitro_sonar::SonarScan::ConstPtr& msg) {
+    sonar_stop = false;
+    if (use_assisted_drive) {
+        last_sonar = msg->header.stamp;
+        std::vector<float> ranges = msg->ranges;
+        for (std::vector<std::string>::size_type i = 0; i < ranges.size(); i++) {
+            if (ranges[i] < SONAR_CLEARING_DIST) {
+                sonar_stop = true;
+                return;
+            }
+        }
+    }
+}
+
 double modulus(double a, double b) {
     int result = static_cast<int>( a / b );
     return a - static_cast<double>( result ) * b;
@@ -117,13 +147,15 @@ int main(int argc, char** argv){
     ros::NodeHandle nh;
     ros::NodeHandle priv_nh("~");
     
-    std::string source_topic, target_topic, costmap_topic;
+    std::string source_topic, target_topic, costmap_topic, sonar_topic;
     
     try {
         priv_nh.getParam("source_topic", source_topic);   
         priv_nh.getParam("target_topic", target_topic);
         priv_nh.getParam("costmap_topic", costmap_topic);
+        priv_nh.getParam("sonar_topic", sonar_topic);
         priv_nh.getParam("clearing_dist", CLEARING_DIST);
+        priv_nh.getParam("sonar_clearing_dist", SONAR_CLEARING_DIST);
         priv_nh.getParam("base_frame_id", BASE_FRAME);
         double diameter;
         nh.getParam("base_diameter", diameter);
@@ -138,6 +170,7 @@ int main(int argc, char** argv){
     
     ros::Subscriber twist_sub = nh.subscribe<geometry_msgs::Twist>(source_topic, 10, twist_cb);
     ros::Subscriber costmap_sub = nh.subscribe<nav_msgs::GridCells>(costmap_topic, 10, costmap_cb);
+    ros::Subscriber sonar_sub = nh.subscribe<mitro_sonar::SonarScan>(sonar_topic, 10, sonar_cb);
     ros::Subscriber cmd_ad_sub = nh.subscribe<std_msgs::Bool>("assisted_drive/set", 10, cmd_ad_cb);
     twist_pub = nh.advertise<geometry_msgs::Twist>(target_topic, 10);
     status_pub = nh.advertise<std_msgs::Bool>("assisted_drive/status", 10);
