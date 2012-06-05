8a4d70886af52a578321d5d054f42ac269a18742
Daan Bloembergen <daan.bloembergen@gmail.com>
2012-06-05 17:49:50 +0000

    fixed start script

--- mitro/mitro_webcontrol/start.sh
+++ mitro/mitro_webcontrol/start.sh
@@ -4,8 +4,7 @@ LOG_FILE="/home/swarmlab/webcontrol.log"
 rm -f $PID_FILE
 rm -f $LOG_FILE
 
-source /opt/ros/electric/setup.bash
-export ROS_MASTER_URI=http://bob:11311
+source /home/swarmlab/.bashrc
 export DISPLAY=:0.0
 
 DONE=0
@@ -13,7 +12,8 @@ while [ $DONE -lt 1 ]; do
     if ps ax | grep skype | grep -v grep > /dev/null
     then
 	sleep 1
-	/home/swarmlab/ros/mitro/mitro_webcontrol/src/webcontrol.py --host 10.10.14.2 --port 8080 &> $LOG_FILE &
+	cd /home/swarmlab/ros/mitro/mitro_webcontrol/src
+	./webcontrol.py --host 10.10.14.2 --port 8080 &
 	ps aux | grep webcontrol.py | grep -v grep | awk '{print $2}' >> $PID_FILE
 	DONE=1
     else
