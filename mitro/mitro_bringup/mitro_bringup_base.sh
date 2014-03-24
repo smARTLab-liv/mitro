5fb50689a9794841e64fcf921aa4e7b5d7d4dbab
Daan Bloembergen <daan.bloembergen@gmail.com>
2014-03-24 14:52:59 +0000

    fix in bringup daemon

--- mitro/mitro_bringup/mitro_bringup_base.sh
+++ mitro/mitro_bringup/mitro_bringup_base.sh
@@ -7,8 +7,9 @@ while [ $DONE -lt 1 ] ; do
 	if rosnode list
 	then
 		sleep 1
-	        roslaunch --pid=/home/swarmlab/mitro_bringup_base.pid mitro_bringup mitro.launch
-    		DONE=1
+	        roslaunch --pid=/home/swarmlab/.ros/mitro_bringup_base.pid mitro_bringup mitro.launch
+    		#roslaunch mitro_bringup mitro.launch
+            DONE=1
 	else
 	        sleep 1
 		echo "no roscore started... waiting"
